487abb09c8caebef80b6a90b6d789aac2d076fc1
NingboGao <931027577@qq.com>
2020-05-19 23:55:34 +0800

    const成员函数调用规则

---
+++ const/const/test.cpp
@@ -0,0 +1,58 @@
+
+#include <iostream>
+using namespace std;
+class Date
+{
+public:
+	void Display()
+	{
+		cout << "Display ()" << endl;
+		cout << "year:" << _year << endl;
+		cout << "month:" << _month << endl;
+		cout << "day:" << _day << endl << endl;
+	}
+	void Display() const
+	{
+		cout << "Display () const" << endl;
+		cout << "year:" << _year << endl;
+		cout << "month:" << _month << endl;
+		cout << "day:" << _day << endl << endl;
+	}
+private:
+	int _year; // 年
+	int _month; // 月
+	int _day; // 日
+};
+void Test()
+{
+	Date d1;
+	d1.Display();
+	const Date d2;
+	d2.Display();
+}
+class Date
+{
+public:
+	Date* operator&()
+	{
+		return this;
+	}
+	const Date* operator&()const
+	{
+		return this;
+	}
+private:
+	int _year; // 年
+	int _month; // 月
+	int _day; // 日
+};//非const对象
+void f2(Date& d)
+{
+	d.Display();
+}
+int main()
+{
+	Date d;
+	f2(d);
+	return 0;
+}
